565e704f80344e17715d1397c0032e62274e6a21
wiranto01 <55836685+wiranto01@users.noreply.github.com>
2019-09-26 18:19:22 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118079/latihan4/kambing/PBOIF210118079Latihan4Kambing.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118079.latihan4.kambing;
+
+/**
+ *
+ * @author 
+ * Nama : Wiranto
+ * Kelas : If2
+ * Nim : 10118079
+ */
+public class PBOIF210118079Latihan4Kambing {
+public void tambahKambing() {
+        // Deklarasi variabel lokal
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        
+        System.out.println("Jumlah Kambing setelah ditambah: " +
+                         jumlahKambing);
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+           PBOIF210118079Latihan4Kambing kambingJantan = new PBOIF210118079Latihan4Kambing();
+        kambingJantan.tambahKambing();        
+
+    }
+    
+}
